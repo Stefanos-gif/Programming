ae6c3afe9ce977650f2fec7f8997d109afe5748e
Stefanos-gif <stefanossiathas@gmail.com>
2024-12-29 16:02:19 +0200

    Improved vector_of_vectors.cpp

--- Cpp_Keywords/vectors/vector_of_vectors.cpp
+++ Cpp_Keywords/vectors/vector_of_vectors.cpp
@@ -2,11 +2,27 @@
 #include <vector>
 
 int main() {
-  std::vector <std::vector<int>> v = {
-  {1,2,3},
-    {4,5,6},
-    {7,8,9}
-  };
+  std::vector <std::vector<int>> v ;
+  std::cout << "Welcome to your own vector matrix!!!\n" << std::endl;
+  int rows;
+  std::cout << "Enter number of rows: ";
+  std::cin >> rows;
+  int place;
+  std::cout << "Enter number of places in each row: ";
+  std::cin >> place;
+  v.resize(rows);
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < place; j++) {
+      int num;
+      std::cout << "Enter number: ";
+      std::cin >> num;
+      v[i].push_back(num);
+    }
+    std::cout << i+2 << " row=>" << std::endl;
+  }
+
+
 
   std::cout << "Matrix form of vector: " << std::endl;
   for (int i = 0; i < v.size(); i++) {
